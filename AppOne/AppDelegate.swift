520faf0cb4cfc719087f7d51ce39e29afbfb0998
Personal <personal@macbook-pro-de-personal.local>
2015-11-29 23:07:30 -0600

    Initial Commit

---
+++ AppOne/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AppOne
+//
+//  Created by Personal on 29/11/15.
+//  Copyright © 2015 Personal. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AppOne/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AppOne
+//
+//  Created by Personal on 29/11/15.
+//  Copyright © 2015 Personal. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
